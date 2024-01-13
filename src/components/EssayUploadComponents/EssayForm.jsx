cdb74bef24c54746b31cfd60d18b3947059fa0ec
Padraic Reilly <patrick.reilly22@gmail.com>
2024-01-13 14:05:19 -0800

    Revert "Update EssayForm.jsx"
    This reverts commit 0193c457b600b6c235ae18cddf80208e9d64c0b1.

--- src/components/EssayUploadComponents/EssayForm.jsx
+++ src/components/EssayUploadComponents/EssayForm.jsx
@@ -23,7 +23,7 @@ function EssayForm({ essayExists, initialTitle, initialSections, onSubmit}) {
                 newSection.data = {
                     ...newSection.data,
                     title: '',
-                    number: sections.filter(s => s.type === 'Chapter').length + 1,
+                    number: sections.filter(s => s.type === 'Chapter').length,
                     youtubeLink: '',
                     // _id: null
                 };
@@ -37,19 +37,19 @@ function EssayForm({ essayExists, initialTitle, initialSections, onSubmit}) {
     };
 
     const updateSectionData = (index, key, value) => {
-        const updatedSections = sections.map((section, i) => {
+        setSections(prevSections => prevSections.map((section, i) => {
             if (i === index) {
                 const updatedData = { ...section.data };
                 if (key === 'pdf' && value) {
-                    updatedData[key] = { name: value.name, file: value };
+                    // Create a new File object to ensure we're not storing a live state
+                    updatedData[key] = new File([value], value.name, { type: value.type });
                 } else {
                     updatedData[key] = value;
                 }
                 return { ...section, data: updatedData };
             }
             return section;
-        });
-        setSections(prevSections => updatedSections)
+        }));
     };
 
     const handleSubmit = (e) => {
