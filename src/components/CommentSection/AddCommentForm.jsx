c5d626290b36f9252ee64ce54686e2b3c4704a2c
Padraic Reilly <patrick.reilly22@gmail.com>
2024-01-27 19:53:55 -0800

    bugfix in handleSubmit()

--- src/components/CommentSection/AddCommentForm.jsx
+++ src/components/CommentSection/AddCommentForm.jsx
@@ -9,48 +9,53 @@ function AddCommentForm({ parentComment, addCommentToList, setParentComment }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const parentCommentId = parentComment._id;
+      let parentCommentId=null;
+      if (parentComment) {
+        parentCommentId = parentComment._id;
+        
+      }
       const response = await postComment({
         text,
         parentCommentId
       });
-      if (response && !response.error) {
-        setText('');
-        addCommentToList(response.data)
-      } else {
-        // Handle any errors, maybe show an error message to the user.
-      }
-    } catch (error) {
-      console.log("Failed to submit comment because: ", error);
+      
+    if (response && !response.error) {
+      setText('');
+      addCommentToList(response.data)
+    } else {
+      // Handle any errors, maybe show an error message to the user.
     }
-  };
+  } catch (error) {
+    console.log("Failed to submit comment because: ", error);
+  }
+};
 
-  return (
-    <Box component="div" sx={{ width: '100%', mb: 3 }}>
-      <Typography variant="body1">
-        Note: All comments must be approved before being displayed.
-      </Typography>
-      <div>
-        {parentComment && <p>Replying to: '{parentComment.text}'</p>}
-        {parentComment && <IconButton  onClick={()=>setParentComment(null)}><Cancel/></IconButton>}
-      </div>
-      <FormControl component="form" onSubmit={handleSubmit} sx={{ mt: 2, width: '100%' }}>
-        <TextField
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="Add a comment..."
-          multiline
-          rows={5}
-          variant="outlined"
-          fullWidth
-          sx={{ backgroundColor: "white", borderRadius: '4px' }}
-        />
-        <Button type="submit" variant="contained" color="primary" sx={{ mt: 2 }}>
-          Submit Comment
-        </Button>
-      </FormControl>
-    </Box>
-  );
+return (
+  <Box component="div" sx={{ width: '100%', mb: 3 }}>
+    <Typography variant="body1">
+      Note: All comments must be approved before being displayed.
+    </Typography>
+    <div>
+      {parentComment && <p>Replying to: '{parentComment.text}'</p>}
+      {parentComment && <IconButton onClick={() => setParentComment(null)}><Cancel /></IconButton>}
+    </div>
+    <FormControl component="form" onSubmit={handleSubmit} sx={{ mt: 2, width: '100%' }}>
+      <TextField
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Add a comment..."
+        multiline
+        rows={5}
+        variant="outlined"
+        fullWidth
+        sx={{ backgroundColor: "white", borderRadius: '4px' }}
+      />
+      <Button type="submit" variant="contained" color="primary" sx={{ mt: 2 }}>
+        Submit Comment
+      </Button>
+    </FormControl>
+  </Box>
+);
 }
 
 export default AddCommentForm;
