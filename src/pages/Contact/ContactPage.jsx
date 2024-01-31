6340e6b59a7625682fe7694c6f6d095fbd183496
Padraic Reilly <patrick.reilly22@gmail.com>
2024-01-30 19:00:17 -0800

    styling, getting ready for form, imports

--- src/pages/Contact/ContactPage.jsx
+++ src/pages/Contact/ContactPage.jsx
@@ -1,12 +1,15 @@
-import { useEffect, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
+import { Button, TextField, Typography, Box, FormControl, IconButton } from '@mui/material';
 import { TitleContext } from "../../components/TitleBar/TitleContext";
-
-export default function ContactPage(){
+import './ContactPage.css'
+export default function ContactPage() {
     const { setTitle } = useContext(TitleContext);
     useEffect(() => {
         setTitle('Contact Information');
     }, [setTitle]);
     return (
-        <p className="contact-info">If you would like to, you may email me regarding these works at <em>OswaldPortrait@gmail.com </em></p>
+        <div className="contact-info">
+            <p>If you would like to, you may email me regarding these works at <em>OswaldPortrait@gmail.com</em>, or send me an email from this form. Your account information will be attached to it.</p>
+        </div>
     );
 }
